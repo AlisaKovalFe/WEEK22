427f782097c6bb8857a6f042d942fc096d5de30e
Alisa <alisakovalhome@mail.ru>
2023-01-19 22:52:45 +0300

    Add correct

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-let user = undefined;
+// let user = undefined;
 
-try {
-    console.log(`Hi, ${user.name}`);
-}
-catch(err) {
-    console.log('Имя пользователя не заполнено');
-}
+// try {
+//     console.log(`Hi, ${user.name}`);
+// }
+// catch(err) {
+//     console.log('Имя пользователя не заполнено');
+// }
 
-try {
-    let json = JSON.parse(`{"age":33}`);
-    if (!json.name) {
-        throw new SyntaxError('Данные неполные')
-    }
-    console.log(json.name);
-}
-catch(err) {
-    console.log('Некорректно введены данные. Подробности: ' + err.message);
-}
+// try {
+//     let json = JSON.parse(`{"age":33}`);
+//     if (!json.name) {
+//         throw new SyntaxError('Данные неполные')
+//     }
+//     console.log(json.name);
+// }
+// catch(err) {
+//     console.log('Некорректно введены данные. Подробности: ' + err.message);
+// }
 
-try {
-    let a = parseInt('ыыы')
-    if (a !=Number) {
-        throw Error('это не число')
-    }
-    console.log(a);
-}
-catch(err) {
-    console.log('Не могу преобразовать: ' + err.message)
-}
+// try {
+//     let a = parseInt('ыыы')
+//     if (a !=Number) {
+//         throw Error('это не число')
+//     }
+//     console.log(a);
+// }
+// catch(err) {
+//     console.log('Не могу преобразовать: ' + err.message)
+// }
+
